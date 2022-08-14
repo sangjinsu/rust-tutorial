3825d46761b9ad6c1faa9db187210dd90d9cf02e
sangjinsu <climbplant39@kakao.com>
2022-08-14 19:14:57 +0900

    generic trait lifecycle

---
+++ generic_trait_lifecycle/src/main.rs
@@ -0,0 +1,45 @@
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewArticle {
+    fn summarize(&self) -> String {
+        format!("{}, {}, {}", self.headline, self.author, self.location)
+    }
+}
+
+fn main() {}
